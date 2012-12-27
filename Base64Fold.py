abe78c97d362e6e34c4cecf422f85c0184c03e3f
DaQuirm <leo.daquirm@gmail.com>
2012-12-27 16:48:49 +0300

    Add plug-in source

---
+++ Base64Fold.py
@@ -0,0 +1,19 @@
+import re
+import sublime, sublime_plugin
+
+class FoldBase64Command(sublime_plugin.TextCommand):
+  def run(self, edit):
+    regions = self.view.find_all('(?<=base64\,)[\w\d\+\/=]+(?=\);)')
+    if regions:
+      self.view.fold(regions)
+
+class Base64Fold(sublime_plugin.EventListener):
+  def on_load(self, view):
+    style_files_exts = ['.css', '.less', '.scss']
+    file_name = view.file_name()
+    if file_name:
+      match = re.search('\.[0-9a-z]+$', file_name, re.IGNORECASE)
+      if match:
+        extension = match.group(0)
+        if extension in style_files_exts:
+          view.run_command('fold_base64')
